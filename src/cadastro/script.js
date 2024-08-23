26708a67b1fba6f4eafea1f2d80f7a69274bdc1a
marcos <marcos23456@discente.ufg.br>
2024-08-22 21:14:36 -0300

    last commit

---
+++ src/cadastro/script.js
@@ -0,0 +1,10 @@
+var botao = document.querySelector('#cadastrar');
+botao.addEventListener('mousedown', function () {
+    botao.style.backgroundColor = 'rgb(54, 199, 54)';
+});
+botao.addEventListener('mouseup',()=>{
+
+    botao.style.backgroundColor = 'rgb(48, 172, 48);'
+
+
+})
---
+++ src/cadastro/script.ts
@@ -0,0 +1,9 @@
+let botao = document.querySelector('#cadastrar') as HTMLInputElement;
+
+botao.addEventListener('mousedown',()=>{
+
+botao.style.backgroundColor = 'blue'
+
+
+
+})
